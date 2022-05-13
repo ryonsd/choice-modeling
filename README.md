9dc127630a45b2a3c4780f4e81b7bcfc077c5df3
ryonsd <ryo.nishida.t4@dc.tohoku.ac.jp>
2022-05-13 14:22:19 +0900

    add main to mnl.py

--- README.md
+++ README.md
@@ -1,17 +1,14 @@
 # Choice Modeling
-Modeling of choice behavior based on Random Utility Theory.
+Modeling of choice behavior based on Random Utility Theory implemented using Python.
 
 ## Models
 * Multinomial Logit Model (MNL)
-  * scratch
-  * xlogit
-* Mixed Multinomial Logit Model (MXL)
-  * xlogit
 * Nested Logit Model (NL)
-  * scratch
-* TasteNet-MNL
-  * [Han+, 2020]
+* TasteNet-MNL [Han+, 2020]
+
+For Mixed Logit Model (MXL) and Latent Class Logit Model (LCCM), I recommend using the Python package xlogit or the R packages mlogit and flemix.
 
 ## References
-* Arteaga, C., Park, J., Beeramoole, P. B., & Paz, A. (2022). xlogit: An open-source Python package for GPU-accelerated estimation of Mixed Logit models. Journal of Choice Modelling, 42, 100339. https://doi.org/10.1016/j.jocm.2021.100339
+* Tetsuro Hyodo. "How to estimate discrete choice models with R" https://www2.kaiyodai.ac.jp/~hyodo/Logit_by_R.pdf
 * Han, Yafei, Christopher Zegras, Francisco Camara Pereira, and Moshe Ben-Akiva. 2020. “A Neural-Embedded Choice Model: TasteNet-MNL Modeling Taste Heterogeneity with Flexibility and Interpretability.” arXiv [econ.EM]. arXiv. http://arxiv.org/abs/2002.00922.
+
--- src/mnl.py
+++ src/mnl.py
@@ -5,7 +5,8 @@ from scipy import stats
 from scipy import optimize
 import numdifftools
 
-
+#=================================================================
+# class for Multinomial Logit Model
 class MNL:
     def __init__(self, data, factor, choice="choice", alt="alt"):
         self.data = data
@@ -17,6 +18,8 @@ class MNL:
         self.n_chid = int(len(self.data) / self.n_alt)
         self.n_factor = len(self.factor)
     
+    #-------------------------------------------------------------------
+    # Log-likelihood   
     def LL(self, beta):
 
         # params
@@ -37,6 +40,8 @@ class MNL:
 
         return -LL_  
     
+    #-------------------------------------------------------------------
+    # Log-likelihood when parameters values are zero
     def LL0(self, beta):
 
         # params
@@ -57,6 +62,8 @@ class MNL:
 
         return -LL_
     
+    #-------------------------------------------------------------------
+    # list of coefficents names
     def coefficients(self):
         coeff = self.data[self.alt].unique()[1:self.n_alt].tolist() 
         for i in range(self.n_alt-1):
@@ -64,35 +71,33 @@ class MNL:
         coeff = coeff + self.factor 
         return coeff
     
+    #-------------------------------------------------------------------
+    # estimates model and print results
     def run(self, beta):
         # about data
         print("number of choice")
         print(self.data[self.data[self.choice] == True].groupby(self.alt).count()[self.choice])
         
         # estimate
+        print("")
+        print("estimating...")
         result = optimize.minimize(self.LL, beta, method="L-BFGS-B")
         result0 = optimize.minimize(self.LL0, beta, method="L-BFGS-B")
 
         beta_opt = result.x
         hess = numdifftools.core.Hessian(mnl.LL)(beta_opt)
         stdev = np.sqrt(np.diagonal(np.linalg.inv(hess)))
-        # ヘッセ行列の逆行列の対角要素の平方根＝標準誤差
 
         LL = -result.fun
         LL0 = -result0.fun
         
         # evaluate
-        n_beta = self.n_alt - 1 + self.n_factor
-        print()
-        print("Evaluation index")
-        print("log-Likelihood:", LL)
-        print("McFadden R2:", 1-(LL/LL0))
-        print("Adjusted McFadden R2:", 1-((LL-n_beta)/LL0))    
+        n_beta = self.n_alt - 1 + self.n_factor 
     
         t = np.zeros(n_beta)
         p = np.zeros(n_beta)
         signif_codes = []
-        N = len(data_wide)
+        N = len(self.data.chid.unique())
         for i in range(n_beta):
             t[i] = beta_opt[i] / stdev[i]
             alpha = stats.t.cdf(abs(t[i]), df=N-1)
@@ -115,8 +120,27 @@ class MNL:
                      "p-value": p,
                      "": signif_codes})
         
-        print()
+        print("-"*80)
         print("Estimated parameters")
         print(summary)
         print()
-        print("Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1")
+        print("Signif. codes:  0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1")
+        
+        print("-"*80)
+        print("Evaluation index")
+        print("log-Likelihood:", LL)
+        print("McFadden R2:", 1-(LL/LL0))
+        print("Adjusted McFadden R2:", 1-((LL-n_beta)/LL0))   
+
+########################################################################
+if __name__ == '__main__':
+    # A data "Mode" from R package "mlogit" is used
+    # Choose a transportation from "car", "carpool", "bus", and "rail" based on "cost" and "time".
+    data_long = pd.read_csv("../data/mode_long.csv", index_col=0) 
+
+    mnl = MNL(data=data_long, factor=["cost", "time"])
+
+    num_params_estimated = 5
+    beta0 = np.zeros(num_params_estimated)
+
+    mnl.run(beta0)
